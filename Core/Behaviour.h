42d8d838c7061163b82423a01d05146ecc184fae
Eurora <35517703+Eurora@users.noreply.github.com>
2018-01-17 15:21:27 +0100

    Add files via upload

---
+++ Core/Behaviour.h
@@ -0,0 +1,17 @@
+/*
+Copyright 2018 Eurora Technologies, all rights reserved
+Author: Samuel Alonso
+Contact: somethingghanging@gmail.com
+Date: 17/01/2018(dd/mm/yyyy)
+Version: 1
+*/
+#pragma once
+
+namespace Eurora
+{
+	void Start();
+
+	void Update();
+
+	void Finish();
+}
