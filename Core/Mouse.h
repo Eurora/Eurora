2381b8d8fe5c4b7610144c4ebc2408a188e00b15
Eurora <35517703+Eurora@users.noreply.github.com>
2018-01-17 07:30:03 +0100

    Add files via upload

--- Core/Mouse.h
+++ Core/Mouse.h
@@ -2,7 +2,7 @@
 Copyright 2018 Eurora Technologies, all rights reserved
 Author: Samuel Alonso
 Contact: somethingghanging@gmail.com
-Date: 17/01/2018(dd/mm/yyyy)
+Date: 16/01/2018(dd/mm/yyyy)
 Version: 1.01
 */
 #pragma once
@@ -11,12 +11,16 @@ Version: 1.01
 
 namespace Eurora
 {
+	//The updated Mouse Position
+	U16 MousePosition[2] = {};
 	//Returns 1 if left-button of the mouse is pressed
 	Bit LeftButton();
 	//Returns 1 if mid-button of the mouse is pressed
 	Bit MidButton();
 	//Returns 1 if right-button of the mouse is pressed
 	Bit RightButton();
-	//Returns mouse position on screen coordinates
-	[2]U16 MousePosition();
+	//Returns Mouse Wheel delta value
+	S16 MouseWheel();
+	//Returns mouse position on screen coordinates to the variable MousePosition
+	void UpdateMousePosition();
 }
